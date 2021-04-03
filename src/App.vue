bfc4762a2b1f34ea7b8312a1e05c034f0738e2af
samansum3 <saman.sum@khalibre.com>
2021-04-03 20:40:45 +0700

    request method test and add bootrsap 4 to app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn btn-primary" @click="sendTestRequest">Send</button>
+    <button class="btn btn-primary" @click="getInvoices">Get invoices</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      
+    },
+    methods: {
+      sendTestRequest() {
+        axios.post('/post').then(console.log).catch(console.error);
+      },
+      getInvoices() {
+        axios.get('/invoice/get-all').then(console.log).catch(console.error);
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
