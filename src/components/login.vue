28d2c09744262f86181ecd5f095b9db0cecefd81
samansum3 <saman.sum@khalibre.com>
2021-04-04 20:08:18 +0700

    sign up user

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,11 +3,12 @@
         <input type="text" class="form-control" placeholder="Enter email" v-model="user.email" />
         <input type="text" class="form-control" placeholder="Enter password" v-model="user.password" />
         <button class="btn btn-primary" @click="login">Login</button>
+        <button class="btn btn-primary" @click="signup">Sign up</button>
     </div>
 </template>
 
 <script>
-import firebase from 'firebase';    //Not secure yet
+import firebase from 'firebase/app';
 
 export default {
     name: 'Login',
@@ -24,6 +25,9 @@ export default {
             firebase.auth().signInWithEmailAndPassword(this.user.email, this.user.password).then(auth => {
                 console.log(auth);
             }).catch(console.error);
+        },
+        signup() {
+            firebase.auth().createUserWithEmailAndPassword(this.user.email, this.user.password).then(console.log).catch(console.error);
         }
     }
 }
