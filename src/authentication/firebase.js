b16f46d6dbaae0801b40f5a1e7d902a96ca9d5b4
samansum3 <saman.sum@khalibre.com>
2021-04-04 15:00:08 +0700

    init firebase authentication

---
+++ src/authentication/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+export default () => {
+    firebase.initializeApp({
+        apiKey: '',
+        authDomain: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: ''
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import initAuthentication from './authentication/firebase';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
+//init firebase auth
+initAuthentication();
+
+//bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
