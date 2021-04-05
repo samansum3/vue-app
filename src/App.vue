5990f124aacd9a8d9a4d3b8d4ddfebde4fb13372
samansum3 <saman.sum@khalibre.com>
2021-04-05 22:26:45 +0700

    make request with firebase user token

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,14 @@
         <div>Hello world</div>
       </div>
     </template>
+    <button class="btn btn-primary" @click="getAllInvoices">Get Incoices</button>
   </div>
 </template>
 
 <script>
   import login from './components/login';
   import firebase from 'firebase/app';
+  import axios from 'axios';
 
   export default {
     name: 'App',
@@ -43,6 +45,19 @@
       });
     },
     methods: {
+      getAllInvoices() {
+        const user = firebase.auth().currentUser;
+        if (!user) {
+          return;
+        }
+        user.getIdToken(false).then(token => {
+          axios.get('/invoice/get-all', {
+            headers: {
+              token: token
+            }
+          }).then(response => console.log(response.data)).catch(console.error);
+        }).catch(console.error);
+      },
       logout() {
         firebase.auth().signOut();
       }
