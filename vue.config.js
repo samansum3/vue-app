dc07ef01855e8e475a5b81472634e30365b687ae
samansum3 <saman.sum@khalibre.com>
2021-04-18 23:10:56 +0700

    chung files to smaller size

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
-// module.exports = {
+module.exports = {
 //     devServer: {
 //         proxy: 'https://invoice-express-app.herokuapp.com'
 //     }
-// }
+    configureWebpack:{
+        performance: {
+        hints: false
+        },
+        optimization: {
+        splitChunks: {
+            minSize: 10000,
+            maxSize: 250000,
+        }
+        }
+    }
+}
