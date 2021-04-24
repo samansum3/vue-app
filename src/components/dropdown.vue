1af46213efa226c14826774c6a912f3841130f68
samansum3 <saman.sum@khalibre.com>
2021-04-24 17:35:14 +0700

    create dropdown select component

---
+++ src/components/dropdown.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="dropdown-wrapper">
+        <div
+            :id="target"
+            class="form-control dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+        >
+            {{ getSelectedLabel }}
+        </div>
+        <div class="dropdown-menu" :aria-labelledby="target">
+            <div v-for="(item, index) in items" :key="index">
+                <a class="dropdown-item unselectable" @click="change(item)">{{ item.value }}</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Dropdown',
+    props: {
+        value: { Type: [Number, String], default: 0 },
+        items: { type: Array, default: () => ([]) },
+        placeholder: { type: String, default: '' },
+        requiredItem: { type: Boolean, default: false }
+    },
+    data() {
+        return {
+            target: 'dropdown' + String(Math.random()).substring(2),
+            selected: null
+        }
+    },
+    computed: {
+        selectedItem() {
+            return this.selected || this.items.find(item => item.key == this.value);
+        },
+        getSelectedLabel() {
+            return this.selectedItem ? this.selectedItem.value : this.placeholder;
+        }
+    },
+    methods: {
+        change(item) {
+            this.selected = item;
+            this.$emit('change', this.requiredItem ? item : item.key);
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -15,7 +15,9 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 firebaseWrapper.initializeApp();
 
 //bootstrap
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
