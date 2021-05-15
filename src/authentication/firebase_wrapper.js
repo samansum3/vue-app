e88f099612a0c2aa09025669ed80ce5cefdd14d9
samansum3 <saman.sum@khalibre.com>
2021-05-15 11:08:33 +0700

    remove un-used keys from front-end code

--- src/authentication/firebase_wrapper.js
+++ src/authentication/firebase_wrapper.js
@@ -4,11 +4,7 @@ import 'firebase/auth';
 const initializeApp = () => {
     firebase.initializeApp({
         apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-        authDomain: process.env.VUE_APP_FIREBASE_DOMAIN,
-        projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.VUE_APP_FIREBASE_APP_ID
+        authDomain: process.env.VUE_APP_FIREBASE_DOMAIN
     });
     firebase.auth().createUserWithEmailAndPassword = () => new Promise(resolve => { resolve(); });
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
