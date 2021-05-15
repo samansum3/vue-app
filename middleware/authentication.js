f30f6f0515198c06d19616e5e3c1fd58684acb22
samansum3 <saman.sum@khalibre.com>
2021-05-15 13:26:39 +0700

    workaround: missing user uid some time latere after logged in

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,6 +3,7 @@ const firebaseAuthentication = (admin) => {
         console.log('fireabse middleware is called');
         const sessionCookie = req.cookies.session || '';
         admin.auth().verifySessionCookie(sessionCookie, true /** checkRevoked */).then(decodedClaims => {
+            req.session.uid = decodedClaims.uid; //TODO fix session expiration instead
             next();
         }).catch(error => {
             res.status(200).json({success: false, message: 'Unauthorize'});
